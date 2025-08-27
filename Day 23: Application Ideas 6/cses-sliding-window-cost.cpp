f781d9e5575583816e02eac79374ecac3f5f4c67
Shivam Singh <xeroid.space@gmail.com>
2025-08-28 03:42:32 +0530

    Some changes

---
+++ Day 23: Application Ideas 6/cses-sliding-window-cost.cpp
@@ -0,0 +1,131 @@
+// Try to do it this way some time:
+// keeping left.size() = right.size() or left.size() + 1 = right.size()
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl "\n"
+
+class BlackBox
+{
+    multiset<int> front, back;
+    int frontSum = 0, backSum = 0;
+
+    void rebalance()
+    {
+        if (front.size() > back.size() + 1)
+        {
+            auto it = prev(front.end(), 1);
+            int it_val = *it;
+            front.erase(it);
+            frontSum -= it_val;
+            back.insert(it_val);
+            backSum += it_val;
+        }
+
+        if (front.size() < back.size())
+        {
+            auto it = back.begin();
+            int it_val = *it;
+            back.erase(it);
+            backSum -= it_val;
+            front.insert(it_val);
+            frontSum += it_val;
+        }
+    }
+
+    int getMedian()
+    {
+        return *front.rbegin();
+    }
+
+public:
+    void insert(int num)
+    {
+        if (front.empty())
+        {
+            front.insert(num);
+            frontSum += num;
+        }
+        else if (num <= *front.rbegin())
+        {
+            front.insert(num);
+            frontSum += num;
+        }
+        else
+        {
+            back.insert(num);
+            backSum += num;
+        }
+
+        rebalance();
+    }
+
+    void remove(int num)
+    {
+        auto front_it = front.find(num);
+        if (front_it != front.end())
+        {
+            front.erase(front_it);
+            frontSum -= num;
+        }
+        else
+        {
+            auto back_it = back.find(num);
+            if (back_it != back.end())
+            {
+                back.erase(back_it);
+                backSum -= num;
+            }
+        }
+
+        rebalance();
+    }
+
+    int getCost()
+    {
+        int median = getMedian();
+        int frontSize = front.size();
+        int backSize = back.size();
+
+        return (median * frontSize - frontSum) + (backSum - median * backSize);
+    }
+};
+
+void solve()
+{
+    int N, K;
+    cin >> N >> K;
+
+    int arr[N];
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    // Solution starts here
+    BlackBox bb;
+    for (int i = 0; i < N; i++)
+    {
+        bb.insert(arr[i]);
+        if (i >= K)
+        {
+            bb.remove(arr[i - K]);
+        }
+
+        if (i >= K - 1)
+        {
+            cout << bb.getCost() << " ";
+        }
+    }
+    cout << endl;
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    solve();
+    return 0;
+}
